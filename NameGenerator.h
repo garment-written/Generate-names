cf4d5866730a23d660ef36f080c8131f012dfaf7
Isabella Schmidt <153085884+garment-written@users.noreply.github.com>
2023-12-06 19:09:15 +0700

    Create NameGenerator.h

---
+++ NameGenerator.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+class NameGenerator {
+public:
+    NameGenerator();
+    std::string generateName();
+
+private:
+    // You can include other necessary data members or methods here
+};
